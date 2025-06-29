4f6afea0bebf2350d2739d8fb16cb6b905138e97
Jziumo <zijin2020@qq.com>
2025-06-29 11:06:32 -0700

    update

--- src/data_process.py
+++ src/data_process.py
@@ -40,6 +40,11 @@ def remove_missing_and_blank_values(df):
 
 
 def read_labeled_data(path_to_data):
+    """
+    Read the data based on a given path.
+    Here we have to handle various cases. 
+    """
+
     filename = os.path.basename(path_to_data)
     suffix = os.path.splitext(filename)[1][1:]
     
@@ -51,15 +56,51 @@ def read_labeled_data(path_to_data):
 
     # df['sentiment_for_product'] = df['sentiment_for_product'].astype('int64')
     # df['sentiment_for_video'] = df['sentiment_for_video'].astype('int64')
-
     df['sentiment_for_product'] = df['sentiment_for_product'].astype('str')
     df['sentiment_for_video'] = df['sentiment_for_video'].astype('str')
 
+    mapping = {
+        '1.0': 'positive',
+        '0.0': 'neutral',
+        '-1.0': 'negative',
+        '1': 'positive',
+        '0': 'neutral',
+        '-1': 'negative',
+        'positive': 'positive', 
+        'neutral': 'neutral',
+        'negative': 'negative'
+    }
+
+    # Apply the mapping to the specified column
+    df['sentiment_for_product'] = df['sentiment_for_product'].map(mapping)
+    df['sentiment_for_video'] = df['sentiment_for_video'].map(mapping)
+
     return df
 
+def read_all_labeled_data(): 
+    """
+    Read all data files under './data/comment_labeled/' directory.   
+    """
+    dir = './data/comment_labeled'
+    
+    files = os.listdir(dir)
+    df_list = []
+
+    for file in files: 
+        df = read_labeled_data(os.path.join(dir, file))
+        df_list.append(df)
+
+    return pd.concat(df_list)
+        
+def check_df(df): 
+    print(f'The size of dataset: {df.shape}', end='\n\n')
+    print(df.head(), end='\n\n')
+    print(df['sentiment_for_product'].value_counts(), end='\n\n')
+    print(df['sentiment_for_video'].value_counts(), end='\n\n')
 
 if __name__ == '__main__': 
     # df = read_labeled_data('./data/comment_labeled/labeled_yamaha_p_225_piano_review_better_music.txt')
     # print(df['sentiment_for_video'].value_counts())
-    read_labeled_data('./data/comment_labeled/labeled_yamaha_p_225_piano_review_better_music.txt')
-    
+    # df = read_labeled_data('./data/comment_labeled/labeled_yamaha_p_225_piano_review_better_music.txt')
+    df = read_all_labeled_data()
+    # check_df(df)
